a0043e7d9885ecce0dcc8a8568058be04997e48d
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-31 08:01:10 -0500

    hecho

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -13,7 +13,13 @@ def test_01():
     try:
         subprocess.run(
             [
-                "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
+                "mlflow",
+                "run",
+                "https://github.com/jdvelasq/mlflow-wine-quality.git",
+                "-e",
+                "knn",
+                "-P",
+                "n_neighbors=5",
             ],
             check=True,
         )

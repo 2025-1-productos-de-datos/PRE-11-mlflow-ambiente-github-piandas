aeaa55234e8baa3899eee5e1106e8863024362ed
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-31 08:23:28 -0500

    test

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -11,23 +11,13 @@ def test_01():
 
     # Test if the homework script runs without errors
     try:
-        result = subprocess.run(
+        subprocess.run(
             [
-                "mlflow",
-                "run",
-                "https://github.com/jdvelasq/mlflow-wine-quality.git",
-                "-e",
-                "knn",
-                "-P",
-                "n_neighbors=5",
+                "mlflow run https://github.com/jdvelasq/mlflow-wine-quality.git -e knn -P n_neighbors=5"
             ],
             check=True,
-            capture_output=True,
-            text=True,
         )
     except subprocess.CalledProcessError as e:
-        print("STDOUT:", e.stdout)
-        print("STDERR:", e.stderr)
         raise Exception(f"Error running the homework script: {e}")
 
     # Ensure the mlruns directory exists
